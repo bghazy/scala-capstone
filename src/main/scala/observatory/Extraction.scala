efff5ffd192e6b70814b9c6376abe79879cbb5ff
Your Name <you@example.com>
2017-11-13 14:18:28 +0100

    week1 + week2

--- src/main/scala/observatory/Extraction.scala
+++ src/main/scala/observatory/Extraction.scala
@@ -1,9 +1,7 @@
 package observatory
 
 import java.time.LocalDate
-
 import scala.io.Source
-import scala.util.Try
 
 /**
   * 1st milestone: data extraction
@@ -18,11 +16,13 @@ object Extraction {
     */
   def locateTemperatures(year: Year, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Temperature)] = {
     val temperatures = parseFile(temperaturesFile).flatMap(parseTempsLine)
-    val stations:Map[StationKey, Location] = parseFile(stationsFile).flatMap(parseStationsLine).toMap
-    def toCelcus(f:Double):Double = BigDecimal((f - 32.0) * (5.0/9.0)).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+    val stations: Map[StationKey, Location] = parseFile(stationsFile).flatMap(parseStationsLine).toMap
 
-    temperatures.flatMap(
-      rec=>  Try((LocalDate.of(year, rec.month, rec.day), stations(rec.key), toCelcus(rec.temp))).toOption
+
+    def toCelsius(f: Double): Double = (f - 32.0) / 1.8
+
+    temperatures.filter(rec => stations.contains(rec.key)).map(
+      rec => (LocalDate.of(year, rec.month, rec.day), stations(rec.key), toCelsius(rec.temp))
     ).toSeq
   }
 
@@ -31,24 +31,25 @@ object Extraction {
     * @return A sequence containing, for each location, the average temperature over the year.
     */
   def locationYearlyAverageRecords(records: Iterable[(LocalDate, Location, Temperature)]): Iterable[(Location, Temperature)] = {
-    def avg(xs: List[Double]) = BigDecimal(xs.sum / xs.length).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
-    records.groupBy(_._2).map{
-      record=>(record._1, avg(record._2.map(_._3).toList))
-    }.toSeq
+    def avg(xs: List[Double]) = xs.sum / xs.length
+
+    records.groupBy(_._2).par.map {
+      record => (record._1, avg(record._2.map(_._3).toList))
+    }.toList
   }
 
-  def parseFile(fileName:String) :Iterator[String] = {
+  def parseFile(fileName: String): Iterator[String] = {
     Source.fromInputStream(getClass.getResourceAsStream(fileName)).getLines()
   }
 
   /**
     * Any Non-numeric input results in the key's component  being None
     *
-    * @param stnStr   STN number as string or the empty string
-    * @param wbanStr  WBAN number as string or the empty string
-    * @return         StationKey
+    * @param stnStr  STN number as string or the empty string
+    * @param wbanStr WBAN number as string or the empty string
+    * @return StationKey
     */
-  def parseStationKey(stnStr: String, wbanStr: String):StationKey =
+  def parseStationKey(stnStr: String, wbanStr: String): StationKey =
     StationKey(toInt(stnStr), toInt(wbanStr))
 
   def toInt(s: String): Option[Int] = {
@@ -62,20 +63,24 @@ object Extraction {
 
   /**
     * Parse a line from a temperatures file
-    * @param str      String of STN,WBAN,MONTH,DAY,TEMP
-    * @return         Parsed line
+    *
+    * @param str String of STN,WBAN,MONTH,DAY,TEMP
+    * @return Parsed line
     */
   def parseTempsLine(str: String): Option[TempsLine] = {
     str.split(",") match {
       case Array(stn, wban, month, day, temp) =>
-        Try(TempsLine(parseStationKey(stn, wban), month.toInt, day.toInt, temp.toDouble)).toOption
+        Option(TempsLine(parseStationKey(stn, wban), month.toInt, day.toInt, temp.toDouble))
       case _ => None
     }
   }
 
-  def parseStationsLine(str:String): Option[(StationKey, Location)] = {
+  def parseStationsLine(str: String): Option[(StationKey, Location)] = {
     str.split(",") match {
-      case Array(stn, wban, lat, lon) => Try((parseStationKey(stn, wban), Location(lat.toDouble, lon.toDouble))).toOption
+      case Array(stn, wban, lat, lon) => {
+        if (lat.length > 0 && lon.length > 0) Option((parseStationKey(stn, wban), Location(lat.toDouble, lon.toDouble)))
+        else None
+      }
       case _ => None
     }
   }
--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -1,38 +1,124 @@
 package observatory
 
+import java.awt.image.BufferedImage
 import com.sksamuel.scrimage.{Image, Pixel}
+import java.lang.Math._
+import scala.annotation.tailrec
 
 /**
   * 2nd milestone: basic visualization
   */
 object Visualization {
 
+  val p = 5.0
+  val distanceThreshold = 1000.0
+  val earthRadius = 6371000.0
+
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
-    * @param location Location where to predict the temperature
+    * @param location     Location where to predict the temperature
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
-    ???
+
+    def distance(a: Location, b: Location): Double = {
+      val dLon = toRadians(abs(a.lon - b.lon))
+      val aLatR = toRadians(a.lat)
+      val bLatR = toRadians(b.lat)
+      val radians = acos(sin(aLatR) * sin(bLatR) + cos(aLatR) * cos(bLatR) * cos(dLon))
+      earthRadius * radians
+    }
+
+    val weights = temperatures.par.map {
+      case (loc, _) => {
+        val d = distance(loc, location)
+        if (d > distanceThreshold)
+          1.0 / pow(d, p)
+        else
+          1.0
+      }
+    }
+    val sumOfWeights = weights.sum
+    val sumOfWeightedTemps = temperatures.par.zip(weights).map {
+      case ((loc, temp), weight) =>
+        val d = distance(loc, location)
+        if (d > distanceThreshold) {
+          weight * temp
+        } else
+          temp
+    }.sum
+
+    sumOfWeightedTemps / sumOfWeights
   }
 
   /**
     * @param points Pairs containing a value and its associated color
-    * @param value The value to interpolate
+    * @param value  The value to interpolate
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Temperature, Color)], value: Temperature): Color = {
-    ???
+    def findColor(hi: Int, lo: Int, t: Double): Int = {
+      if (hi == lo) hi
+      else abs((lo * (1.0 - t) + hi * t + 0.5).toInt)
+    }
+
+    def findBounds(points: Iterable[(Temperature, Color)], value: Temperature): ((Temperature, Color), (Temperature, Color)) = {
+      @tailrec
+      def findBoundsRec(points: Iterable[(Temperature, Color)], value: Temperature,
+                        hi: (Temperature, Color), lo: (Temperature, Color)): ((Temperature, Color), (Temperature, Color)) = {
+        points match {
+          case Nil => (hi, lo)
+          case p :: tail => {
+            if (p._1 == value) (p, p)
+            else if (p._1 > value) (hi, p)
+            else findBoundsRec(tail, value, p, p)
+          }
+        }
+      }
+      findBoundsRec(points, value, points.head, points.head)
+    }
+
+    val (hi, lo) = findBounds(points.toArray.sortBy(_._1).toList, value)
+    if (hi == lo)
+      hi._2
+    else {
+      val t = Math.abs((value - lo._1) / (hi._1 - lo._1))
+      Color(
+        findColor(hi._2.red, lo._2.red, t),
+        findColor(hi._2.green, lo._2.green, t),
+        findColor(hi._2.blue, lo._2.blue, t))
+
+    }
   }
 
   /**
     * @param temperatures Known temperatures
-    * @param colors Color scale
+    * @param colors       Color scale
     * @return A 360×180 image where each pixel shows the predicted temperature at its location
     */
-  def visualize(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)]): Image = {
-    ???
-  }
+  def visualize(temperatures: Iterable[(Location, Double)],
+                colors: Iterable[(Double, Color)]): Image = {
 
+    val imgType = BufferedImage.TYPE_INT_RGB
+    val width = 360
+    val height = 180
+
+    def findIndexToLocation(i: Int, rowWidth: Int): Location = {
+      val rowIndex = i / rowWidth
+      val colIndex = i - (rowIndex * rowWidth)
+      val lat = if (rowIndex <= 90) 90 - rowIndex else (rowIndex - 90) * -1
+      val lon = if (colIndex <= 180) colIndex - 180 else colIndex - 180
+      Location(lat, lon)
+    }
+
+    val pixels = Stream.range(0, width * height).par
+      .map(findIndexToLocation(_, width))
+      .map(predictTemperature(temperatures, _))
+      .map(interpolateColor(colors, _))
+      .map(color => Pixel(color.red, color.green, color.blue, 255))
+      .toArray
+
+    Image(width, height, pixels, imgType)
+  }
 }
 
